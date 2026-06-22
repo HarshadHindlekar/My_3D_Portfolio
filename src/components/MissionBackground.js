c4c9853725c616f910fe01988c9e13045007af8c
HarshadHindlekar <harshad.hindlekar29@gmail.com>
2026-06-22 12:05:07 +0530

    feat: add floating asteroid background layer

--- src/components/MissionBackground.js
+++ src/components/MissionBackground.js
@@ -5,6 +5,11 @@ export const MissionBackground = () => {
     <div className="mission-background" aria-hidden="true">
       <StarsCanvas />
       <div className="mission-background__grid" />
+      <div className="mission-background__asteroids">
+        {Array.from({ length: 9 }, (_, index) => (
+          <span key={index} />
+        ))}
+      </div>
       <div className="mission-background__orbit mission-background__orbit--one" />
       <div className="mission-background__orbit mission-background__orbit--two" />
       <div className="mission-background__planet-glow" />
