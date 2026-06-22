1d536267749ac3bbbf67fcf57e0c5c037aa97cad
HarshadHindlekar <harshad.hindlekar29@gmail.com>
2026-06-22 11:05:45 +0530

    fix: use transparent mission cutout assets

--- src/components/MissionVisual.js
+++ src/components/MissionVisual.js
@@ -1,9 +1,25 @@
-import missionVisuals from '../assets/img/mission-section-visuals.png';
+import systemsAstronaut from '../assets/img/mission-cutouts/systems-astronaut.png';
+import projectsAstronaut from '../assets/img/mission-cutouts/projects-astronaut.png';
+import signalsAstronaut from '../assets/img/mission-cutouts/signals-astronaut.png';
+import contactAstronaut from '../assets/img/mission-cutouts/contact-astronaut.png';
+
+const missionVisuals = {
+  systems: systemsAstronaut,
+  projects: projectsAstronaut,
+  signals: signalsAstronaut,
+  contact: contactAstronaut,
+};
 
 export const MissionVisual = ({ variant, className = '', alt = '' }) => {
+  const src = missionVisuals[variant];
+
+  if (!src) {
+    return null;
+  }
+
   return (
     <div className={`mission-visual mission-visual--${variant} ${className}`.trim()} aria-hidden={alt ? undefined : true}>
-      <img src={missionVisuals} alt={alt} loading="lazy" />
+      <img src={src} alt={alt} loading="lazy" />
     </div>
   );
 };
